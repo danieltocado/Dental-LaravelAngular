27b94c7b3c37cc04d8024dce2c023111710df8b4
danieltocado2 <danieltocado2@gmail.com>
2020-08-06 21:15:16 +0200

    [Frontend] Added new routes for treatments.

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -4,12 +4,22 @@ import { HomeComponent } from './components/home/home.component'
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
+import { TratamientoOrtodonciaComponent } from './components/tratamiento-ortodoncia/tratamiento-ortodoncia.component';
+import { TratamientoEsteticadentalComponent } from './components/tratamiento-esteticadental/tratamiento-esteticadental.component';
+import { TratamientoMedicinabucalComponent } from './components/tratamiento-medicinabucal/tratamiento-medicinabucal.component';
+import { TratamientoOdontopediatriaComponent } from './components/tratamiento-odontopediatria/tratamiento-odontopediatria.component';
+import { TratamientoOtrosComponent } from './components/tratamiento-otros/tratamiento-otros.component';
 
 const routes: Routes = [
   {path:"", component: HomeComponent},
   {path:"register", component: RegisterComponent},
   {path:"login", component: LoginComponent},
-  {path:"calendar", component: CalendarComponent}
+  {path:"calendar", component: CalendarComponent},
+  {path:"tratamiento/ortodoncia", component: TratamientoOrtodonciaComponent},
+  {path:"tratamiento/estetica-dental", component: TratamientoEsteticadentalComponent},
+  {path:"tratamiento/medicina-bucal", component: TratamientoMedicinabucalComponent},
+  {path:"tratamiento/odontopediatria", component: TratamientoOdontopediatriaComponent},
+  {path:"tratamiento/otros", component: TratamientoOtrosComponent}
 ];
 
 @NgModule({
