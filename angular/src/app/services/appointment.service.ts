e108932a5cf84d6977f57952b513b53d7518bdf4
danieltocado2 <danieltocado2@gmail.com>
2020-08-08 22:00:15 +0200

    [Frontend] Created Appointment service.

---
+++ angular/src/app/services/appointment.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient} from '@angular/common/http'
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment'
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router'
+import { Appointment } from '../models/appointment.model';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppointmentService {
+
+  API_URL:string = environment.API_URL;
+  public appointment: Appointment;
+  public appointments: Appointment[] = [];
+  constructor(private HttpClient: HttpClient) { }
+
+  create(appointment: Appointment): Observable<Appointment> {
+    console.log(appointment);
+    return this.HttpClient.post<Appointment>(this.API_URL + '/api/appointment/create', appointment);
+  }
+
+  setAppointment(appointment: Appointment):void {
+    this.appointment = appointment;
+  }
+
+  getAppointment(): Appointment {
+    return this.appointment;
+  }
+
+  getAppointments():Observable<any> {
+    return this.HttpClient.get(this.API_URL+'/api/appointment');
+  }
+
+  setAppointments(appointments:Appointment[]):void {
+
+    this.appointments = appointments;
+  }
+
+  getAppointmentsB(): Appointment[] {
+    return this.appointments;
+  }
+
+}
